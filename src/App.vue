4865f7f80288a3b8c5ed2e81117839685a298911
Dmitriy <dexan.abriel@gmail.com>
2019-08-06 21:43:03 +0300

    work version

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <ReTable :tableData="tableData"></ReTable>
+  </div>
+</template>
+
+
+<script>
+import ReTable from './components/ReTable.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ReTable,
+  },
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        test: 'sdfgsdf',
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        test: 'sdfgsdf',
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        test: 'sdfgsdf',
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        test: 'sdfgsdf',
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        test: 'sdfgsdf',
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ReTable.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <table class="table table-bordered table-dark">
+        <thead>
+          <tr v-for="(column, key) in tableData[1]" :key="key">
+            <th>{{ key }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(column) in tableData" :key="column.id">
+            <td v-for="row in column" :key="row">
+              {{ row }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ReTable',
+  props: [
+    'tableData',
+  ],
+  data() {
+    return {
+    };
+  },
+  computed: {
+    columns() {
+      return Object.keys(this.tableData[1]).length;
+    },
+  },
+};
+</script>
+
+<style>
+@import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+
+</style>
